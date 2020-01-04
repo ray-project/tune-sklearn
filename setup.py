9b15fc43ed2e550ddc806b1f2fce7c5d8df08a85
Michael Chau <inventormc@gmail.com>
2020-01-03 22:15:45 -0800

    version for sklearn

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.1',
     packages=['tune_sklearn'],
     author='Michael Chau/Anthony Yu',
-    install_requires=['scikit-learn', 'scipy', 'ray', 'numpy']
+    install_requires=['scikit-learn>=0.22', 'scipy', 'ray', 'numpy']
 )
