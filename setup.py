4a9bf141857fa76aa42d8f9464be6ebb4e9c5917
Michael Chau <inventormc@gmail.com>
2020-03-13 11:09:24 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name='tune-sklearn',
+    name='tune_sklearn',
     version='0.1',
     packages=['tune_sklearn'],
     author='Michael Chau/Anthony Yu',
-    install_requires=['scikit-learn>=0.22', 'scipy', 'ray', 'numpy']
+    description='An experimental scikit-learn API on Tune',
+    url='https://github.com/ray-project/tune-sklearn',
+    install_requires=['scikit-learn>=0.22', 'scipy', 'ray', 'numpy>=1.16', 'pandas', 'tabulate', 'parameterized']
 )
