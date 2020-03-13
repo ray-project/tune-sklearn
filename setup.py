2407ece11b952d9613e519a238ef50dad94dae5b
Michael Chau <inventormc@gmail.com>
2020-03-13 11:35:01 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from distutils.core import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name='tune_sklearn',
-    version='0.1',
     packages=['tune_sklearn'],
     author='Michael Chau/Anthony Yu',
     description='An experimental scikit-learn API on Tune',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description='An API enabling faster scikit-learn training using Tune parallelization and early stopping algorithms',
     url='https://github.com/ray-project/tune-sklearn',
     install_requires=['scikit-learn>=0.22', 'scipy', 'ray', 'numpy>=1.16', 'pandas', 'tabulate', 'parameterized']
 )
