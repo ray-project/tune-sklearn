7764eee9236c8a4d427822d400ae1d63700b4df8
Michael Chau <inventormc@gmail.com>
2020-03-13 11:21:11 -0700

    long description

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='tune_sklearn',
     version='0.1',
     packages=['tune_sklearn'],
     author='Michael Chau/Anthony Yu',
     description='An experimental scikit-learn API on Tune',
+    long_description=long_description,
     url='https://github.com/ray-project/tune-sklearn',
     install_requires=['scikit-learn>=0.22', 'scipy', 'ray', 'numpy>=1.16', 'pandas', 'tabulate', 'parameterized']
 )
