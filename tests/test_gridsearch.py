19bd6ce47f4005c56032397026db356c392f498b
Michael Chau <inventormc@gmail.com>
2020-07-12 15:27:38 -0700

    lint

--- tests/test_gridsearch.py
+++ tests/test_gridsearch.py
@@ -566,10 +566,7 @@ class GridSearchTest(unittest.TestCase):
             "C": [1, 10, 100, 1000]
         }
 
-        tune_search = TuneGridSearchCV(
-            SVC(),
-            tuned_parameters,
-            max_iters=20)
+        tune_search = TuneGridSearchCV(SVC(), tuned_parameters, max_iters=20)
         tune_search.fit(X_train, y_train)
 
         pred = tune_search.predict(X_test)
