7c0091fa7a052f87d656a72708290b53fc553c37
Michael Chau <inventormc@gmail.com>
2020-07-15 17:19:54 -0700

    syntax

--- tune_sklearn/_trainable.py
+++ tune_sklearn/_trainable.py
@@ -13,6 +13,7 @@ import ray.cloudpickle as cpickle
 import warnings
 from joblib import Parallel, delayed
 
+
 class _Trainable(Trainable):
     """Class to be passed in as the first argument of tune.run to train models.
 
@@ -21,22 +22,16 @@ class _Trainable(Trainable):
 
     """
 
-    def _partial_fit(self, i, train, test)
-        X_train, y_train = _safe_split(self.estimator[i], self.X,
-                                       self.y, train)
+    def _partial_fit(self, i, train, test):
+        X_train, y_train = _safe_split(self.estimator[i], self.X, self.y,
+                                       train)
         X_test, y_test = _safe_split(
-            self.estimator[i],
-            self.X,
-            self.y,
-            test,
-            train_indices=train)
-        self.estimator[i].partial_fit(X_train, y_train,
-                                      np.unique(self.y))
+            self.estimator[i], self.X, self.y, test, train_indices=train)
+        self.estimator[i].partial_fit(X_train, y_train, np.unique(self.y))
         if self.return_train_score:
-            self.fold_train_scores[i] = self.scoring(
-                self.estimator[i], X_train, y_train)
-        self.fold_scores[i] = self.scoring(self.estimator[i], X_test,
-                                           y_test)
+            self.fold_train_scores[i] = self.scoring(self.estimator[i],
+                                                     X_train, y_train)
+        self.fold_scores[i] = self.scoring(self.estimator[i], X_test, y_test)
 
     def _setup(self, config):
         """Sets up Trainable attributes during initialization.
@@ -94,7 +89,10 @@ class _Trainable(Trainable):
 
         """
         if self.early_stopping is not None:
-            Parallel(n_jobs=-1)(delayed(self._partial_fit)(i, train, test) for i, (train, test) in enumerate(self.cv.split(self.X, self.y)))
+            Parallel(n_jobs=-1)(
+                delayed(self._partial_fit)(i, train, test)
+                for i, (train,
+                        test) in enumerate(self.cv.split(self.X, self.y)))
 
             ret = {}
             total = 0
