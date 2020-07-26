b3ee9211079697827ecb4394fb4c7bbe73f08ac4
Michael Chau <inventormc@gmail.com>
2020-07-26 12:55:41 -0700

    bug fix

--- tune_sklearn/tune_basesearch.py
+++ tune_sklearn/tune_basesearch.py
@@ -258,7 +258,7 @@ class TuneBaseSearchCV(BaseEstimator):
         self.cv = cv
         self.scoring = scoring
         self.n_jobs = n_jobs
-        if os.environ.get("sk_n_jobs") is not None:
+        if os.environ.get("SKLEARN_N_JOBS") is not None:
             self.sk_n_jobs = int(os.environ.get("SKLEARN_N_JOBS"))
         else:
             self.sk_n_jobs = sk_n_jobs
