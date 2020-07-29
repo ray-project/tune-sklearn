5f2f829b06804b39920012d41219434c89a1e591
Michael Chau <inventormc@gmail.com>
2020-07-28 19:55:47 -0700

    lint

--- examples/multimetric.py
+++ examples/multimetric.py
@@ -12,7 +12,6 @@ from sklearn.linear_model import SGDClassifier
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from ray.tune.schedulers import MedianStoppingRule
-import numpy as np
 
 digits = datasets.load_digits()
 x = digits.data
@@ -25,7 +24,11 @@ parameter_grid = {"alpha": [1e-4, 1e-1, 1], "epsilon": [0.01, 0.1]}
 scheduler = MedianStoppingRule(grace_period=10.0)
 
 tune_search = TuneGridSearchCV(
-    clf, parameter_grid, early_stopping=scheduler, max_iters=10, scoring=("accuracy", "f1_micro"), refit="accuracy")
+    clf,
+    parameter_grid,
+    early_stopping=scheduler,
+    max_iters=10,
+    scoring=("accuracy", "f1_micro"),
+    refit="accuracy")
 tune_search.fit(x_train, y_train)
 print(tune_search.cv_results_)
-
