0be3c500f3c53cfcefb054c2c30ba3f45fb3d710
Antoni Baum <antoni.baum@protonmail.com>
2020-08-19 23:11:39 +0200

    New search algorithms (#68)
    Co-authored-by: Richard Liaw <rliaw@berkeley.edu>

--- examples/discrete_hyperopt.py
+++ examples/discrete_hyperopt.py
@@ -2,6 +2,7 @@ from tune_sklearn import TuneSearchCV
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from hyperopt import hp
 
 digits = datasets.load_digits()
 X = digits.data
@@ -10,9 +11,9 @@ y = digits.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 space = {
-    "n_estimators": (100, 200),
+    "n_estimators": hp.uniformint("n_estimators", 100, 200),
     "min_weight_fraction_leaf": (0.0, 0.5),
-    "min_samples_leaf": (1, 5)
+    "min_samples_leaf": hp.uniformint("min_samples_leaf", 1, 5)
 }
 
 tune_search = TuneSearchCV(
