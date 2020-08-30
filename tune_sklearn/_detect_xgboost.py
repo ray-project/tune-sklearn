5009199c3b95cd5d2b917fa8d339e4d94fab4366
Richard Liaw <rliaw@berkeley.edu>
2020-08-30 16:05:57 -0700

    fix-xgboost

--- tune_sklearn/_detect_xgboost.py
+++ tune_sklearn/_detect_xgboost.py
@@ -1,12 +1,13 @@
 def has_xgboost():
     try:
-        import xgboost  # ignore: F401
+        import xgboost  # noqa: F401
         return True
     except ImportError:
         return False
 
+
 def is_xgboost_model(clf):
     if not has_xgboost():
         return False
-    import xgboost  # ignore: F401
+    from xgboost.sklearn import XGBModel  # noqa: F401
     return isinstance(clf, XGBModel)
