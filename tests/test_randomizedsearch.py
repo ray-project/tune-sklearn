fbf9b330db60aeb628bf76a0b4bff2aec7f9e93b
Michael Chau <inventormc@gmail.com>
2020-09-05 17:49:13 -0700

    fix test

--- tests/test_randomizedsearch.py
+++ tests/test_randomizedsearch.py
@@ -165,7 +165,7 @@ class RandomizedSearchTest(unittest.TestCase):
     def test_warm_start_detection(self):
         parameter_grid = {"alpha": Real(1e-4, 1e-1, 1)}
         from sklearn.ensemble import VotingClassifier
-        clf = VotingClassifier(max_depth=2, random_state=0)
+        clf = VotingClassifier()
         tune_search = TuneSearchCV(
             clf,
             parameter_grid,
