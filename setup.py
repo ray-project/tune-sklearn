e865bce84833c169e317b5d4c3268069d41ddb6a
Richard Liaw <rliaw@berkeley.edu>
2020-09-11 16:28:16 -0700

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="tune_sklearn",
     packages=["tune_sklearn"],
-    version="0.0.8",
+    version="0.1.0",
     author="Michael Chau/Anthony Yu",
     description="An experimental scikit-learn API on Tune",
     long_description="An API enabling faster scikit-learn training using Tune "
