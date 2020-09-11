06acae206ecfe97aef6cfea922d1e7c3ba7b4cac
Michael Chau <inventormc@gmail.com>
2020-09-10 22:07:31 -0700

    small change sklearn setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name="tune_sklearn",
     packages=["tune_sklearn"],
-    version="0.0.7",
+    version="0.0.8",
     author="Michael Chau/Anthony Yu",
     description="An experimental scikit-learn API on Tune",
     long_description="An API enabling faster scikit-learn training using Tune "
     "parallelization and early stopping algorithms",
     url="https://github.com/ray-project/tune-sklearn",
     install_requires=[
-        "scikit-learn>=0.22", "scipy", "ray", "numpy>=1.16", "pandas",
+        "scikit-learn", "scipy", "ray", "numpy>=1.16", "pandas",
         "tabulate", "parameterized"
     ])
