abad88495036fde72f88ea41b556f77992a00155
Richard Liaw <rliaw@berkeley.edu>
2020-12-17 18:50:23 -0800

    Try version bump + fix version again (#165)

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-from setuptools import setup
+import io
+import os
+from setuptools import setup, find_packages
+
+ROOT_DIR = os.path.dirname(__file__)
+
+# Ray tests depend on buidling tune_sklearn. Thus, if `setup.py`
+# depends on Ray, then we create a cyclic dep.
+# workaround from: https://stackoverflow.com/a/17626524
+with open("tune_sklearn/_version.py") as f:
+    text = f.readlines()  # Returns ['__version__ = "0.2.0"']
+    __version__ = text[-1].split()[-1].strip("\"'")
+
+VERSION = os.environ.get("TSK_RELEASE_VERSION", __version__)
 
 setup(
     name="tune_sklearn",
-    packages=["tune_sklearn"],
-    version="0.0.8",
-    author="Michael Chau/Anthony Yu",
-    description="An experimental scikit-learn API on Tune",
-    long_description="An API enabling faster scikit-learn training using Tune "
-    "parallelization and early stopping algorithms",
+    packages=find_packages(),
+    version=VERSION,
+    author="Michael Chau, Anthony Yu, and Ray Team",
+    author_email="ray-dev@googlegroups.com",
+    description=("A drop-in replacement for Scikit-Learn’s "
+                 "GridSearchCV / RandomizedSearchCV with cutting edge "
+                 "hyperparameter tuning techniques."),
+    long_description=io.open(
+        os.path.join(ROOT_DIR, "README.md"), "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/ray-project/tune-sklearn",
     install_requires=["scikit-learn", "scipy", "ray[tune]", "numpy>=1.16"])
--- tune_sklearn/__init__.py
+++ tune_sklearn/__init__.py
@@ -1,4 +1,5 @@
 from tune_sklearn.tune_gridsearch import TuneGridSearchCV
 from tune_sklearn.tune_search import TuneSearchCV
+from tune_sklearn._version import __version__
 
-__all__ = ["TuneGridSearchCV", "TuneSearchCV"]
+__all__ = ["TuneGridSearchCV", "TuneSearchCV", "__version__"]
