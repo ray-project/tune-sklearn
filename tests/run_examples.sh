454b6667bcb4bebd9b0aecb6066bc0d57778e255
Richard Liaw <rliaw@berkeley.edu>
2021-02-12 09:42:21 -0800

    new-script
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

---
+++ tests/run_examples.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Cause the script to exit if a single command fails
+set -eo pipefail
+builtin cd "$(dirname "${BASH_SOURCE:-$0}")"
+ROOT="$(git rev-parse --show-toplevel)"
+builtin cd "$ROOT/examples"
+# rm catboostclassifier.py
+rm bohb_example.py hpbandster_sgd.py # Temporary hack to avoid breaking CI
+for f in *.py; do echo "running $f" && python "$f" || exit 1 ; done
+
