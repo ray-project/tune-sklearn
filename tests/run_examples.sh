fff1cda67d3beb487d9114c7606bb7bf5f5f049f
Richard Liaw <rliaw@berkeley.edu>
2021-02-12 18:37:14 -0800

    fix
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- tests/run_examples.sh
+++ tests/run_examples.sh
@@ -6,7 +6,8 @@ set -eo pipefail
 builtin cd "$(dirname "${BASH_SOURCE:-$0}")"
 ROOT="$(git rev-parse --show-toplevel)"
 builtin cd "$ROOT/examples"
+PYTHON="${PYTHON:-python}"
 # rm catboostclassifier.py
 rm bohb_example.py hpbandster_sgd.py # Temporary hack to avoid breaking CI
-for f in *.py; do echo "running $f" && python "$f" || exit 1 ; done
+for f in *.py; do echo "running $f" && $PYTHON "$f" || exit 1 ; done
 
