67af5a931b9ed9cc7d75fa892d7146a40e569c1c
Antoni Baum <antoni.baum@protonmail.com>
2022-04-22 20:29:22 +0200

    Bump version to 0.4.3 (#243)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ ROOT_DIR = os.path.dirname(__file__)
 # depends on Ray, then we create a cyclic dep.
 # workaround from: https://stackoverflow.com/a/17626524
 with open("tune_sklearn/_version.py") as f:
-    text = f.readlines()  # Returns ['__version__ = "0.4.1"']
+    text = f.readlines()  # Returns ['__version__ = "0.4.3"']
     __version__ = text[-1].split()[-1].strip("\"'")
 
 VERSION = os.environ.get("TSK_RELEASE_VERSION", __version__)
@@ -19,7 +19,7 @@ setup(
     version=VERSION,
     author="Michael Chau, Anthony Yu, and Ray Team",
     author_email="ray-dev@googlegroups.com",
-    description=("A drop-in replacement for Scikit-Learn’s "
+    description=("A drop-in replacement for Scikit-Learn's "
                  "GridSearchCV / RandomizedSearchCV with cutting edge "
                  "hyperparameter tuning techniques."),
     long_description=io.open(
