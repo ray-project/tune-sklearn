8033f18d7b385590909951b1ef127956bbc8b1b5
Antoni Baum <antoni.baum@protonmail.com>
2022-10-04 21:02:53 +0000

    Bump version to 0.4.4
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ ROOT_DIR = os.path.dirname(__file__)
 # depends on Ray, then we create a cyclic dep.
 # workaround from: https://stackoverflow.com/a/17626524
 with open("tune_sklearn/_version.py") as f:
-    text = f.readlines()  # Returns ['__version__ = "0.4.3"']
+    text = f.readlines()  # Returns ['__version__ = "0.4.4"']
     __version__ = text[-1].split()[-1].strip("\"'")
 
 VERSION = os.environ.get("TSK_RELEASE_VERSION", __version__)
